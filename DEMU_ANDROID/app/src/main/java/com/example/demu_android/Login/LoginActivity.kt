ff3d03f0572bd02fc085a5a0ca265047c5e86e23
유섭 <yoonbk2001027@gmail.com>
2024-10-09 20:03:21 +0900

    feat :: DataBinding 적용

--- DEMU_ANDROID/app/src/main/java/com/example/demu_android/Login/LoginActivity.kt
+++ DEMU_ANDROID/app/src/main/java/com/example/demu_android/Login/LoginActivity.kt
@@ -1,17 +1,28 @@
 package com.example.demu_android.Login
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.demu_android.R
+import com.example.demu_android.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
 
+        binding.btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        
     }
 }
