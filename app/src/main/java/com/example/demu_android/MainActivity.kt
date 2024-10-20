31fd24edad8984407a7c811fa0d99fcf109189fb
유섭 <yoonbk2001027@gmail.com>
2024-10-20 23:30:32 +0900

    feat :: bottom navtion fragment Transaction 연결

--- app/src/main/java/com/example/demu_android/MainActivity.kt
+++ app/src/main/java/com/example/demu_android/MainActivity.kt
@@ -3,12 +3,50 @@ package com.example.demu_android
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.demu_android.Blog.WrtieBlogFragment
+import com.example.demu_android.Mypage.MyPageFragment
+import com.example.demu_android.Search.SearchFragment
+import com.example.demu_android.databinding.ActivityMainBinding
+import com.example.demu_android.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        
+        setContentView(binding.root)
+
+        setBottomNavigationFragment()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.containers, HomeFragment()).commit()
+        }
+    }
+
+    private fun setBottomNavigationFragment() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.bottom_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.containers, HomeFragment()).commit()
+                    true
+                }
+                R.id.bottom_search -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.bottom_search, SearchFragment()).commit()
+                    true
+                }
+                R.id.bottom_blog -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.bottom_blog, WrtieBlogFragment()).commit()
+                    true
+                }
+                R.id.bottom_my_page -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.bottom_my_page, MyPageFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
