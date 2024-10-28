547f7d835c5fd0dff6ceadc8974f5967e4fd2dac
유섭 <yoonbk2001027@gmail.com>
2024-10-28 16:20:13 +0900

    feat :: retrofit 종속성 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation (libs.retrofit)
+    implementation(libs.converter.gson)
 }
