3a776aea9bc8897c4998ef65038e127ad20e1da5
유섭 <yoonbk2001027@gmail.com>
2024-11-04 21:56:14 +0900

    feat :: BlogReadOnly 뷰 추가 완료

--- app/src/main/java/com/example/demu_android/feature/Mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/demu_android/feature/Mypage/MyPageFragment.kt
@@ -35,9 +35,9 @@ class MyPageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+        return binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
